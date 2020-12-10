c8d3fe97bcc8ba186517f602f7ea4fc4c6b6545b
liangzichao <64008266+liangzichao@users.noreply.github.com>
2020-12-10 22:20:45 +0800

    操作练习

---
+++ Test_demo.py
@@ -0,0 +1,37 @@
+# import pytest
+#
+# #@pytest.mark.parametrize("")
+# class Test_demo(object):
+#
+#
+#     def test_one(self):
+#
+#         a = 1
+#         b = 2
+#         assert a != b
+#
+#         print("这是我的第一个测试用例")
+#
+#     def test_to(self):
+#
+#         a = 1
+#         b = 2
+#         assert a != b
+#
+#         print("这是我的第二个测试用例")
+#
+# if __name__ == '__main__':
+#     pytest.main()
+#
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ python基本语法.py
@@ -0,0 +1,11 @@
+a = 0
+
+if a == 0:
+
+    print("我是零食中超")
+elif a == 1:
+
+    print("我是哈哈哈")
+
+else:
+    print("我是超")
